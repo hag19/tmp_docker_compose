81e4a2607f841b0583f7de219ed1b6ff38f49afa
hag19 <136019174+hag19@users.noreply.github.com>
2024-11-20 20:57:44 +0100

    Delete web/src directory

--- web/src/database/migrations/add-firstname-lastname.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . "/../database/connection.php";
-
-$databaseConnection->query("ALTER TABLE users ADD COLUMN firstname VARCHAR(50) NOT NULL;");
-$databaseConnection->query("ALTER TABLE users ADD COLUMN lastname VARCHAR(50) NOT NULL;");
